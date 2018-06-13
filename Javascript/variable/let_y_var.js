e3bff2b93200b8f3fa55d974cee37e68bb9a73a7
FMMV <fmmedranov@gmail.com>
2018-06-13 17:41:28 -0400

    Acomodos Menores

--- Javascript/variable/let_y_var.js
+++ Javascript/variable/let_y_var.js
@@ -5,19 +5,19 @@
 var numero = 10;
 console.log(numero); //valor 10
 
-if(true){
-	var numero = 50; 
+if (true) {
+	var numero = 50;
 	console.log(numero); //valor 50
 }
 
 console.log(numero); //valor 50
 
 //Prueba con let este solo actua dentro del bloque de llaves
-var texto= "Francisco";
-console.log(texto);// valor Francisco
+var texto = "Francisco";
+console.log(texto); // valor Francisco
 
 if (true) {
-	let texto ="Manuel";
-	console.log(texto);// valor Manuel
+	let texto = "Manuel";
+	console.log(texto); // valor Manuel
 }
-console.log(texto);// valor Francisco
+console.log(texto); // valor Francisco
--- Javascript/variable/variables.js
+++ Javascript/variable/variables.js
@@ -1,17 +1,18 @@
-'use strict' //si lo comento la variable interpreta pero si lo uso debo de definir es lo mejor en este momento.
-			 //Modo Estricto.
+'use strict' 
+//si lo comento la variable interpreta pero si lo uso debo de definir es lo mejor en este momento.
+//Modo Estricto.
 
 //VARIABLES
 /* Una variable es un contenedor de informacion*/
 
- var pais = "Venezuela";
- var continente = "America";
- var antiguedad= 2019; //esto se puede cambiar de manera dinamica
- var pais_y_continente= pais+' '+continente;
- 
+var pais = "Venezuela";
+var continente = "America";
+var antiguedad = 2019; //esto se puede cambiar de manera dinamica
+var pais_y_continente = pais + ' ' + continente;
 
- pais = "Mexico";
- continente = " Latinoamerica";
- 
- console.log(pais,  continente, antiguedad);
- alert(pais_y_continente);
+
+pais = "Mexico";
+continente = " Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
